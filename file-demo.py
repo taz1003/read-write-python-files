8a5574eae24908e93440e64a75153347a22fed46
taz1003 <li9vyrit@students.codeinstitute.net>
2026-06-01 22:20:40 +0200

    Copy image in binary in chunks of 1024 bytes

--- file-demo.py
+++ file-demo.py
@@ -14,11 +14,22 @@
 #             wf.write(line)
 
 
-# 'crimson-desert.png will be copied to crimson-desert_copy.png.
-# The 'rb' mode is used to read the file in binary mode,
-# and the 'wb' mode is used to write the file in binary mode.
-# This is necessary for copying binary files such as images.'
+# # 'crimson-desert.png will be copied to crimson-desert_copy.png.
+# # The 'rb' mode is used to read the file in binary mode,
+# # and the 'wb' mode is used to write the file in binary mode.
+# # This is necessary for copying binary files such as images.'
+# with open("crimson-desert.png", 'rb') as rf:
+#     with open("crimson-desert_copy.png", 'wb') as wf:
+#         for line in rf:
+#             wf.write(line)
+
+
+# The above code reads the image file in chunks of 1024 bytes
+# and writes each chunk to the new file. This is more efficient 
+# than reading and writing the entire file at once, especially 
+# for large files.
 with open("crimson-desert.png", 'rb') as rf:
     with open("crimson-desert_copy.png", 'wb') as wf:
-        for line in rf:
-            wf.write(line)
+        chunk_size = 1024
+        while len(chunk := rf.read(chunk_size)) > 0:
+            wf.write(chunk)
